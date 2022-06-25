47f4d7a8a0260349ed7a0de3ebed4adb7324eb08
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-06-25 19:19:40 +0200

    WIP: Verify OTP function

--- src/yubico/yubico.ts
+++ src/yubico/yubico.ts
@@ -1,3 +1,6 @@
+import * as crypto from 'crypto';
+import * as https from 'https';
+
 import { IYubico } from '../interfaces';
 import { SL } from '../types';
 import { SERVERS } from '../constants';
@@ -59,5 +62,44 @@ export class Yubico {
         this.apiServers = options.apiServers;
       }
     }
+  }
+
+  /**
+   * Verify key against Yubico servers
+   * More info on OTPs https://developers.yubico.com/OTP/OTPs_Explained.html
+   * @param otp {string} The OTP from the YubiKey.
+   * @returns {Promise<Response>}
+   */
+  public async verifyOtp(otp: string): Promise<Response> {
+    // The length of the key = [16, 40]
+    const nonce: string = crypto.randomBytes(16).toString('hex');
+
+    const requestParams: URLSearchParams = new URLSearchParams();
+
+    requestParams.append('id', this.clientId);
+    requestParams.append('nonce', nonce);
+    requestParams.append('otp', otp);
+
+    if (this.sl) {
+      requestParams.append('sl', this.sl as string);
+    }
+
+    if (this.timeout) {
+      requestParams.append('timeout', this.timeout.toString());
+    }
+
+    requestParams.append('timestamp', '1');
+
+    // Sort them to properly allow for the security hash
+    // requestParams.sort();
+
+    const hash = crypto
+      .createHmac('sha1', Buffer.from(this.secretKey, 'base64'))
+      .update(requestParams.toString())
+      .digest('base64');
+
+    requestParams.append('h', hash);
+
+    console.log(requestParams.toString());
   }
 }
